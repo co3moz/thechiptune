3b8b311f0d47e9d07eb98d6254774882b2decf43
Doğan Derya <doganderya59@gmail.com>
2016-03-16 14:39:11 +0200

    Time, seo, glsl prepare

--- src/public/component/main.js
+++ src/public/component/main.js
@@ -1,6 +1,5 @@
 var app = angular.module("theChiptuneApplication", []);
-app.constant('baseHref', '/base/index.html')
-  .value('$sniffer', {history: true});
+app.value('$sniffer', {history: true});
 app.controller("mainController", function ($scope, $http, $location, $browser) {
   var player = new ChiptuneJsPlayer();
   var location = $location.path().split("/");
@@ -23,21 +22,43 @@ app.controller("mainController", function ($scope, $http, $location, $browser) {
           var dot = item.resource.lastIndexOf(".");
           item.attribute = item.resource.substring(dot + 1);
           item.name = item.resource.substring(0, dot);
+          item.url = btoa(item.resource).replace(/=/g,'');
         }
       });
       $scope.items = response.data;
     });
   };
 
+  $scope.numberToMinutes = function(number) {
+    var minutes = number / 60 >> 0;
+    var seconds = number % 60 >> 0;
+    return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+  };
+
   var currentLocationOfMusic = 0.0;
-  var intervalSpeed = 500;
+  var intervalSpeed = 250;
   setInterval(function () {
     if ($scope.isPlaying) {
       $scope.percent = Math.min(100, currentLocationOfMusic / $scope.duration * 100);
+      $scope.currentLocationOfMusic = currentLocationOfMusic;
       currentLocationOfMusic += intervalSpeed / 1000;
       $scope.$apply();
     }
   }, intervalSpeed);
+
+  player.onEnded(function() {
+    currentLocationOfMusic = $scope.duration;
+    $scope.isPlaying = false;
+    if($scope.isAuto) {
+      var index = $scope.orderedItems.findIndex(function(item) {
+        return item == $scope.playingItem;
+      });
+
+      if(index != -1 && index + 2 <= $scope.orderedItems.length) {
+        $scope.enter($scope.orderedItems[index + 1]);
+      }
+    }
+  });
 
   $scope.play = function () {
     if ($scope.isPlaying == false) {
@@ -67,15 +88,17 @@ app.controller("mainController", function ($scope, $http, $location, $browser) {
     if (item.isDirectory == true) {
       return $scope.branch(item.resource);
     } else {
+      player.stop();
       $scope.playing = item.resource;
       $scope.replay = function () {
         player.load("/resource/get/" + location.join("/") + "/" + encodeURI(item.resource), function (buffer) {
           player.config.repeatCount = $scope.isLoop == true ? -1 : 0;
-          player.play(buffer);
-          $browser.url("/" + location.join("/") + "?play=" + encodeURI(item.resource) + ($scope.isLoop == true ? "&loop=true" : ""));
           $scope.isPlaying = true;
+          $scope.playingItem = item;
+          player.play(buffer);
           $scope.duration = player.duration();
           currentLocationOfMusic = 0.0;
+          $browser.url("/" + location.join("/") + "?play=" + btoa(item.resource).replace(/=/g, "") + ($scope.isLoop == true ? "&loop=true" : ""));
         });
       };
 
@@ -106,12 +129,16 @@ app.controller("mainController", function ($scope, $http, $location, $browser) {
   $scope.loop = function (value) {
     $scope.isLoop = value;
     $scope.replay();
-  }
+  };
+
+  $scope.isAuto = false;
+  $scope.auto = function (value) {
+    $scope.isAuto = value;
+  };
 
   if (directPlay) {
-    $scope.enter({resource: directPlay, isDirectory: false});
+    $scope.enter({resource: atob(directPlay), isDirectory: false});
   }
-
 }).config(function ($locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
 });
